4f3ea9c6312f0ed3f2da941c3a912eb66c738168
Teej622 <87628535+Teej622@users.noreply.github.com>
2026-03-27 19:05:08 -0400

    Fixed score bug

--- scenes/game.gd
+++ scenes/game.gd
@@ -4,41 +4,39 @@ extends Node2D
 @onready var scoreType = %ScoreType
 @export var par = [2, 2, 2, 2]
 var stage = 0
-var shots
 var shot_history = []
 var starting_pos = [Vector2(90,80), Vector2(), Vector2(), Vector2()]
 
 func _physics_process(delta: float) -> void:
 	%ShotCounter.text = str(hamster_ball.shots)
-	shots = %ShotCounter.text.to_int()
 	
 	%Par.text = "Par: " + str(par[stage])
 	
 
 func _on_goal_score() -> void:
 	print("score")
-	if shots > par[stage] + 3:
-		scoreType.text = str(shots) + " over par..."
-	elif shots == par[stage] + 3:
-		scoreType.text = "Triple Bogey\n(" + str(shots - par[stage]) +" over par)"
-	elif shots == par[stage] + 2:
-		scoreType.text = "Double Bogey\n(" + str(shots - par[stage]) +" over par)"
-	elif shots == par[stage] + 1:
-		scoreType.text = "Bogey\n(" + str(shots - par[stage]) +" over par)"
-	elif shots == 1:
+	if hamster_ball.shots > par[stage] + 3:
+		scoreType.text = str(hamster_ball.shots) + " over par..."
+	elif hamster_ball.shots == par[stage] + 3:
+		scoreType.text = "Triple Bogey\n(" + str(hamster_ball.shots - par[stage]) +" over par)"
+	elif hamster_ball.shots == par[stage] + 2:
+		scoreType.text = "Double Bogey\n(" + str(hamster_ball.shots - par[stage]) +" over par)"
+	elif hamster_ball.shots == par[stage] + 1:
+		scoreType.text = "Bogey\n(" + str(hamster_ball.shots - par[stage]) +" over par)"
+	elif hamster_ball.shots == 1:
 		scoreType.text = "Hole in One!"
-	elif shots == par[stage]:
+	elif hamster_ball.shots == par[stage]:
 		scoreType.text = "Par"
-	elif shots == par[stage] - 1:
-		scoreType.text = "Birdie!\n(" + str(par[stage] - shots) +" under par)"
-	elif shots == par[stage] - 2:
-		scoreType.text = "Eagle!\n(" + str(par[stage] - shots) +" under par)"
-	elif shots == par[stage] - 3:
-		scoreType.text = "Albatross!\n(" + str(par[stage] - shots) +" under par)"
-	elif shots <= par[stage] - 4:
-		scoreType.text = "Holy F$%k!\n(" + str(par[stage] - shots) +" under par)"
+	elif hamster_ball.shots == par[stage] - 1:
+		scoreType.text = "Birdie!\n(" + str(par[stage] - hamster_ball.shots) +" under par)"
+	elif hamster_ball.shots == par[stage] - 2:
+		scoreType.text = "Eagle!\n(" + str(par[stage] - hamster_ball.shots) +" under par)"
+	elif hamster_ball.shots == par[stage] - 3:
+		scoreType.text = "Albatross!\n(" + str(par[stage] - hamster_ball.shots) +" under par)"
+	elif hamster_ball.shots <= par[stage] - 4:
+		scoreType.text = "Holy F$%k!\n(" + str(par[stage] - hamster_ball.shots) +" under par)"
 	
-	print(shots)
+	print(hamster_ball.shots)
 
 	%ScoreScreen.visible = true
 	
@@ -49,18 +47,18 @@ func _on_next_stage_btn_pressed() -> void:
 	if stage > 4:
 		pass
 	%ScoreScreen.visible = false
-	shot_history.append(shots)
+	shot_history.append(hamster_ball.shots)
 	shot_counter_update()
 	stage_handler()
 	
 func _on_retry_btn_pressed() -> void:
 	%ScoreScreen.visible = false
-	shots = 0
+	hamster_ball.shots = 0
 	shot_counter_update()
 	hamster_ball.position = starting_pos[stage]
 
 func shot_counter_update():
-	%ShotCounter.text = str(shots)
+	%ShotCounter.text = str(hamster_ball.shots)
 
 func stage_handler():
 	var stages: Array[Node] = %Stages.get_children()
